11a12736b370fa47930e2c25323968a824899bb4
hutp <13218063373@163.com>
2018-12-25 17:21:25 +0800

    react react-dom babel依赖

--- src/index.js
+++ src/index.js
@@ -27,26 +27,26 @@ ReactDOM.render(element, document.getElementById('root')); */
       </div>
     );
   }  */
-   class Clock extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Hello, world!</h1>
-          <h2>现在时间 {this.props.date.toLocaleTimeString()}.</h2>
-          <p data-myattribute = "somevalue">这是一个很不错的 JavaScript 库!</p>
-        </div>
-      );
-    }
-  }
-   
-  function tick() {
-    ReactDOM.render(
-      <Clock date={new Date()} />,
-      document.getElementById('root')
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>现在时间 {this.props.date.toLocaleTimeString()}.</h2>
+        <p data-myattribute="somevalue">这是一个很不错的 JavaScript 库!</p>
+      </div>
     );
   }
-   
-setInterval(tick, 1000);   
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
